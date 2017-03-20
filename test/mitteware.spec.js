3639ef1928fffb8e188dca5f078b13234403a730
Michael Cohen <michael.cohen@globality.com>
2017-03-19 23:59:05 -0700

    Switch to using mocha for unit testing

--- test/mitteware.spec.js
+++ test/mitteware.spec.js
@@ -1,13 +1,36 @@
 import { mitte } from '../src/index';
+import isFunction from 'lodash/isFunction';
 
-describe('createMitteware', () => {
+describe('mitte', function () {
 
-    it('exposes public API', () => {
+    it('exposes public API', function () {
         const manager = mitte();
 
-        expect(manager.use).toBeDefined();
-        expect(manager.run).toBeDefined();
-        expect(manager.runForKey).toBeDefined();
+        expect(isFunction(manager.use)).to.be.true;
+        expect(isFunction(manager.run)).to.be.true;
+        expect(isFunction(manager.runForKey)).to.be.true;
+    });
+
+});
+
+describe('foo', function () {
+
+    it('blah', function () {
+        // const manager = mitte();
+
+        // const middleware1 = jest.fn(next => next());
+        // const middleware2 = jest.fn(next => next());
+
+        // manager.use(middleware1);
+        // manager.use(middleware2);
+
+        // const handler = jest.fn();
+
+        // manager.run(handler);
+
+        // expect(middleware1).toHaveBeenCalledTimes(1);
+        // expect(middleware2).toHaveBeenCalledTimes(1);
+        // expect(handler).toHaveBeenCalledTimes(1);
     });
 
 });
